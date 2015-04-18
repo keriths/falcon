4dbb88bcc99d86697b14dad4239b725a20f1e7b3
shuai.fan <shuai.fan@dianping.com>
2015-04-18 11:00:18 +0800

    update

---
+++ falcon-netty/falcon-netty-demo/falcon-netty-demo-customer/src/main/java/com/falcon/demo/action/LoginInterceptor.java
@@ -0,0 +1,31 @@
+package com.falcon.demo.action;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by fanshuai on 15/4/16.
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println(o);
+        return false;
+
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
